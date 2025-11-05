24a5c5401166ae6a46a297168893a25fdf5a53a7
MasterPedidos <mjunior.ribeiro@terra.com.br>
2025-11-05 13:39:39 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+// Mude o nome do cache quando trocar arquivos para forçar atualização
+const CACHE_NAME = 'pedidos-online-v3';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icones/icone-192.png',
+  './icones/icone-512.png',
+  './ativos/logo.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match(req).then(cached => cached || caches.match('./index.html')))
+  );
+});
