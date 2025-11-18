7e19e677751ab6e5045046d4422e99cd8b71c58c
MasterPedidos <mjunior.ribeiro@terra.com.br>
2025-11-18 13:29:28 -0300

    Delete iniciar_servidor.bat

--- iniciar_servidor.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo ================================
-echo  SERVIDOR INICIADO
-echo  Acesse no navegador:
-echo  http://localhost:8080
-echo ================================
-python -m http.server 8080
-pause
